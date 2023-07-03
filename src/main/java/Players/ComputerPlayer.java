9cb6ad53f49fed3adbabe77a995e6249a2df94f7
chondosha1234 <jmiller61193@gmail.com>
2023-07-03 00:10:37 -0400

    created player class and human and computer sublcasses

---
+++ src/main/java/Players/ComputerPlayer.java
@@ -0,0 +1,9 @@
+package Players;
+
+public class ComputerPlayer extends Player {
+
+    public ComputerPlayer(boolean whiteSide) {
+        this.whiteSide = whiteSide;
+        this.humanPlayer = false;
+    }
+}
---
+++ src/main/java/Players/Player.java
@@ -0,0 +1,18 @@
+package Players;
+
+/**
+ * An abstract class for player, can be human or computer
+ */
+public abstract class Player {
+    public boolean whiteSide;
+    public boolean humanPlayer;
+
+    public boolean isWhiteSide() {
+        return this.whiteSide;
+    }
+
+    public boolean isHumanPlayer() {
+        return this.humanPlayer;
+    }
+}
+
