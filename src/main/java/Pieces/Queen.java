54d33d569a07aa5d666e1a69bceee9804ba1277a
chondosha1234 <jmiller61193@gmail.com>
2023-07-05 15:55:31 -0400

    implemented queen moves method

--- src/main/java/Pieces/Queen.java
+++ src/main/java/Pieces/Queen.java
@@ -16,12 +16,93 @@ public class Queen extends Piece {
             return false;
         }
 
-        int x = Math.abs(start.getX() - end.getX());
-        int y = Math.abs(start.getY() - end.getY());
+        int startX = start.getX();
+        int startY = start.getY();
+        int endX = end.getX();
+        int endY = end.getY();
 
-        // combine rook and bishop logic for moves
-        // x / y == 1  or x * y == 0 ?
+        int x = Math.abs(startX - endX);
+        int y = Math.abs(startY - endY);
 
-        return true;
+        // diagonal line movement like bishop
+        if (x / y == 1) {
+            if (startX < endX && startY < endY) {
+                int i = startX + 1;
+                int j = startY + 1;
+                for (; i < endX && j < endY; i++, j++) {
+                    if (board.getBox(i, j).getPiece() != null) {
+                        return false;
+                    }
+                }
+            }
+            if (startX < endX && startY > endY) {
+                int i = startX + 1;
+                int j = startY - 1;
+                for (; i < endX && j > endY; i++, j--) {
+                    if (board.getBox(i, j).getPiece() != null) {
+                        return false;
+                    }
+                }
+            }
+            if (startX > endX && startY < endY) {
+                int i = startX - 1;
+                int j = startY + 1;
+                for (; i > endX && j < endY; i--, j++) {
+                    if (board.getBox(i, j).getPiece() != null) {
+                        return false;
+                    }
+                }
+            }
+            if (startX > endX && startY > endY) {
+                int i = startX - 1;
+                int j = startY - 1;
+                for (; i > endX && j > endY; i--, j--) {
+                    if (board.getBox(i, j).getPiece() != null) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        // horizontal and vertical line movement like rook
+        if (x * y == 0) {
+            // checking to see if there are blocking pieces along y-axis
+            if (x == 0) {
+                // checking only direction of movement
+                if (startY < endY) {
+                    for (int i = startY + 1; i < endY; i++) {
+                        if (board.getBox(startX, i).getPiece() != null) {
+                            return false;
+                        }
+                    }
+                } else {
+                    for (int i = startY - 1; i > endY; i--) {
+                        if (board.getBox(startX, i).getPiece() != null) {
+                            return false;
+                        }
+                    }
+                }
+            } else {
+                //checking for blocking along x-axis
+                if (startX < endX) {
+                    for (int i = startX + 1; i < endX; i++) {
+                        if (board.getBox(i, startY).getPiece() != null) {
+                            return false;
+                        }
+                    }
+                } else {
+                    for (int i = startX - 1; i > endX; i--) {
+                        if (board.getBox(i, startY).getPiece() != null) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        // didn't meet any valid move criteria
+        return false;
     }
 }
